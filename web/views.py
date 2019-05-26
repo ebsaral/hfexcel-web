f437ca804872f8230635438eff7bc061a5ccad3f
Emin Bugra Saral <eminbugrasaral@me.com>
2019-05-26 23:34:10 +0200

    hfexcel: version upgrade 0.0.10

--- web/views.py
+++ web/views.py
@@ -21,7 +21,7 @@ class DocumentManagerView(TemplateResponseMixin, View):
         return self.render_to_response(context)
 
     def post(self, request, *args, **kwargs):
-        version = '0.0.9'
+        version = '0.0.10'
         form = DocumentUploadForm(data=request.POST)
 
         if form.is_valid():
